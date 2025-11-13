8796801d9022b97e92e063ee69109d57d502ed19
PC <PC@DESKTOP-6J7SHM1>
2025-11-13 17:47:40 -0500

    Arreglos en la vista; se agregó la ventana para agregar el producto y artista

---
+++ src/polisongstock/PoliSongStock.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package polisongstock;
+
+
+
+
+/**
+ *
+ * @author PC
+ */
+public class PoliSongStock {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
