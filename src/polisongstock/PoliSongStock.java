f51fbde5c9e0a08b5ce2d38020cb1259a8c9a86b
PC <PC@DESKTOP-6J7SHM1>
2025-11-14 22:43:35 -0500

    Borra esto porfa, solo es la prueba

--- src/polisongstock/PoliSongStock.java
+++ src/polisongstock/PoliSongStock.java
@@ -17,7 +17,8 @@ public class PoliSongStock {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        for (int i = 0; i < 10; i++) 
+            System.out.print("proyecto de mierda");
     }
     
 }
